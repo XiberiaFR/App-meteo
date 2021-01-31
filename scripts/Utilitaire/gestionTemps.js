ae7f78f1d10af4f15837672a37b6ac9e324853d9
Xiberia <xiberiafr.git@protonmail.com>
2021-02-01 00:46:57 +0100

    finalisation

--- scripts/Utilitaire/gestionTemps.js
+++ scripts/Utilitaire/gestionTemps.js
@@ -6,14 +6,14 @@ let ajd = new Date();
 let options = {weekday: 'long'};
 // on utilise le format FR jour/mois/année
 let jourActuel = ajd.toLocaleDateString('fr-FR', options);
-console.log(jourActuel, ajd);
+// console.log(jourActuel, ajd);
 
 // On prend la première lettre du jour actuel, on la met en majuscule et on y ajoute le mot entier à partir de la 2ème lettre via slice(1)
 jourActuel = jourActuel.charAt(0).toUpperCase() + jourActuel.slice(1);
-console.log(jourActuel);
+// console.log(jourActuel);
 
 // on découpe le tableau à partir du jour actuel et on vient lui concaténer le tableau jourSemaine jusqu'au jour d'aujourd'hui  
 let tabJoursEnOrdre = jourSemaine.slice(jourSemaine.indexOf(jourActuel)).concat(jourSemaine.slice(0, jourSemaine.indexOf(jourActuel)));
-console.log(tabJoursEnOrdre);
+// console.log(tabJoursEnOrdre);
 
 export default tabJoursEnOrdre;
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 import tabJoursEnOrdre from './Utilitaire/gestionTemps.js';
 
-console.log('depuis mains js:' + tabJoursEnOrdre);
 const CLEAPI = 'ca7fe76ef3323095b224d0745e22fc77';
 let resultatsAPI;
 
@@ -9,13 +8,17 @@ const temperature = document.querySelector('.temperature');
 const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempsPourH = document.querySelectorAll('.heure-prevision-valeur');
+const joursDiv = document.querySelectorAll('.jour-prevision-nom');
+const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
+const imgIcone = document.querySelector('.logo-meteo');
+const chargementContainer = document.querySelector('.overlay-icone-chargement');
 
 
 // On affiche une demande d'autorisation pour accéder à la localisation via le navigateur
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
         
-        console.log(position);
+        // console.log(position);
         let long = position.coords.longitude;
         let lat = position.coords.latitude;
         AppelAPI(long,lat);
@@ -29,7 +32,7 @@ if(navigator.geolocation) {
 // On crée la fonction qui gère la gestion des données de l'API 
 function AppelAPI(long,lat) {
 
-    console.log(long, lat);
+    // console.log(long, lat);
 
     // On se connecte à l'API pour obtenir les données
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEAPI}`)
@@ -41,7 +44,7 @@ function AppelAPI(long,lat) {
     
     // On sélectionne les données souhaitées et on les affiche dans l'app
     .then((data) => {
-        console.log(data);
+        // console.log(data);
 
         resultatsAPI = data;
 
@@ -76,5 +79,25 @@ function AppelAPI(long,lat) {
         for(let t = 0; t < tempsPourH.length; t++) {
             tempsPourH[t].innerText = Math.trunc(resultatsAPI.hourly[t * 3].temp) + '°c';
         }
+
+        // 3 premières lettres des jours
+        for(let d = 0; d < tabJoursEnOrdre.length; d++) {
+            joursDiv[d].innerText = tabJoursEnOrdre[d].slice(0,3);
+        }
+
+        // temperature par jour
+        for (let m = 0; m < 7; m++){
+            tempJoursDiv[m].innerText = Math.trunc(resultatsAPI.daily[m + 1].temp.day) + '°c';
+        }
+
+        // Icone météo dynamique
+        if(heureActuelle >= 6 && heureActuelle < 21) {
+            imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`
+        } else {
+            imgIcone.src = `ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`
+        }
+
+        chargementContainer.classList.add('disparition');
+
     })
 }
